c165c1fb447834164cb4b3978d3d16bb540e4286
Satyam <satyam18solanki@gmail.com>
2024-06-21 17:17:39 +0530

    Push missing element Program

---
+++ MissingElement.java
@@ -0,0 +1,19 @@
+//array me missing eleent find karna ka program
+class demo
+{
+	public static void main(String ar[])
+	{
+		int anser;
+		int x[]={0,1,2,3,4,5};
+		int sum=0;
+		for(int num:x)
+		{
+			sum+=num; 
+		}
+		int total=x.length*(x.length+1);
+		total/=2;
+		anser=total-sum;
+	System.out.println("missing alement="+anser);		
+	}
+
+}
